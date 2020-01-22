dca167c35b23d7d66b522fac463677039671f3b7
GiancarloZ <gzabaleta48@gmail.com>
2020-01-22 12:15:30 -0500

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,13 @@
 class BirdsController < ApplicationController
+  
   def index
     @birds = Bird.all
     render json: @birds
   end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    render json: bird
+  end
+
 end
