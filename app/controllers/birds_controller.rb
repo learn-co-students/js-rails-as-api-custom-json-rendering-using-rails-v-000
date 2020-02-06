c8703a5f027e67ddb92bb6f7d68d2d1394f33a89
William Barela <william.barela.dev@gmail.com>
2020-02-06 16:45:20 -0600

    Rails rendering JSON is amazing

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,22 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    # render json: birds, only: [:id, :name, :species]
+    render json: birds, except: [:created_at, :updated_at]
+    # Line 5 is Rails magic for the following in Ruby:
+    # render json: birds.to_json(except: [:created_at, :updated_at])
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    # render json: {id: bird.id, name: bird.name, species: bird.species}
+    # a Ruby shorthand for doing the above on a single hash:
+    # render json: bird.slice(:id, :name, :species)
+    # Rails magic to do the same thing (only and except can be used)
+    if bird
+      render json: bird, except: [:created_at, :updated_at]
+    else
+      render json: { message: 'That bird ain\'t there' }
+    end
   end
 end
