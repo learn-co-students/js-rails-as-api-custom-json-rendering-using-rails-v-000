3dc4b8a8752c45792a978626cd669d4c4cd97c04
Dana ONeil <doneil1580@gmail.com>
2020-02-15 20:31:30 -0500

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,19 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds, only: [:id, :name, :species]
+    # render json: birds, except: [:Created_at, :updated_at]
   end
+
+  # basic error messaging when rendering JSON data
+  def show
+    #  if record is not found it will return nil(false-y value)
+    bird = Bird.find_by(id: params[:id])
+    # to cover error messages
+    if bird 
+      render json: { id: bird.id, name: bird.name, bird.species }
+    else 
+      render json: { message: 'Bird not found' }
+    end 
+  end 
 end
