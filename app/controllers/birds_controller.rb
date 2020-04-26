302f091f9e285204f858feb12ba0128fcd26ada4
gigi <mmgamao1@gmail.com>
2020-04-26 12:27:38 -0700

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,4 +3,9 @@ class BirdsController < ApplicationController
     @birds = Bird.all
     render json: @birds
   end
+
+  def index
+    birds = Bird.all
+    render json: birds, except: [:created_at, :updated_at]
+  end
 end
