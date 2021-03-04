c021610844a55759b92cf1a780f57a96a091b4b9
github username <d24dust@hotmail.com>
2021-03-04 14:16:55 -0500

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,15 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds, only: [:id, :name, :species]
+    # Works like slice, except for collections
+    # fetch(/birds) => [{id:1, name: ""...}, {id:2, name: ""...}]
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    render json: bird.slice(:id, :name, :species)
+    # It will only return a hash with :name and :email
+    # fetch(users/1) => {name: "John", email: "yourface@idk.com"}
   end
 end
